075d365b4e7e2e2b82e0bf2001f5c025970b4fbc
백재원 <bjw1622@gmail.com>
2023-09-15 15:50:48 +0900

    feat : finish guestbook03

---
+++ guestbook03/src/main/java/com/poscodx/guestbook/controller/GuestbookController.java
@@ -0,0 +1,51 @@
+package com.poscodx.guestbook.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.poscodx.guestbook.repository.GuestbookRepository;
+import com.poscodx.guestbook.vo.GuestbookVo;
+
+@Controller
+public class GuestbookController {
+	@Autowired
+	private GuestbookRepository guestbookRepository;
+
+	@RequestMapping("/")
+	public String main(Model model) {
+		List<GuestbookVo> list = guestbookRepository.findAll();
+		model.addAttribute("list", list);
+		return "index";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String add(GuestbookVo vo) {
+		guestbookRepository.insert(vo);
+		return "redirect:/";
+	}
+
+	@RequestMapping(value = "/delete/{no}", method = RequestMethod.GET)
+	public String delete(@PathVariable("no") Long no, Model model) {
+		model.addAttribute("no", no);
+		return "deleteform";
+	}
+
+	@RequestMapping(value = "/delete/{no}", method = RequestMethod.POST)
+	public String delete(@PathVariable("no") Long no,
+			@RequestParam(value = "password", required = true, defaultValue = "") String password,Model model) {
+		
+		model.addAttribute("no", no);
+		guestbookRepository.deleteByNoAndPassword(no, password);
+		return "redirect:/";
+	}
+
+}
